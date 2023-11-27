9b409b63b7c5d661f9f7ff486bcc0fbdc1159b77
Michael Swann <n1020368@my.ntu.ac.uk>
2023-11-27 21:23:09 +0000

    Can move between login and signup fragments

--- app/src/main/java/com/example/hypermile/AuthenticationActivity.java
+++ app/src/main/java/com/example/hypermile/AuthenticationActivity.java
@@ -1,23 +1,29 @@
 package com.example.hypermile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
 //    Button goToSignup;
+    TextView loginLink;
+    TextView signupLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
-
         loadFragment(new LoginFragment());
     }
-    private void loadFragment(Fragment fragment) {
+    protected void loadFragment(Fragment fragment) {
 // create a FragmentManager
         FragmentManager fragmentManager = getFragmentManager();
 // create a FragmentTransaction to begin the transaction and replace the
--- app/src/main/java/com/example/hypermile/LoginFragment.java
+++ app/src/main/java/com/example/hypermile/LoginFragment.java
@@ -1,20 +1,30 @@
 package com.example.hypermile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 public class LoginFragment extends android.app.Fragment {
     View view;
-    Button firstButton;
+    TextView signupLink;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-// Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_login, container, false);
-// get the reference of Button
+        signupLink = (TextView) view.findViewById(R.id.signUpLink);
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthenticationActivity authenticationActivity = (AuthenticationActivity) getActivity();
+                authenticationActivity.loadFragment(new SignupFragment());
+            }
+        });
 
         return view;
     }
