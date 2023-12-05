801dd5f6011b0854a2d7b7744e9d95b71adce452
Michael Swann <n1020368@my.ntu.ac.uk>
2023-12-05 12:08:40 +0000

    Error message shows on login screen

--- app/src/main/java/com/example/hypermile/LoginFragment.java
+++ app/src/main/java/com/example/hypermile/LoginFragment.java
@@ -1,15 +1,19 @@
 package com.example.hypermile;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 public class LoginFragment extends android.app.Fragment {
     View view;
@@ -18,12 +22,14 @@ public class LoginFragment extends android.app.Fragment {
     Button loginButton;
     TextView signupLink;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_login, container, false);
         loginButton = (Button) view.findViewById(R.id.loginBtn);
         usernameField = (EditText) view.findViewById(R.id.username);
         passwordField = (EditText) view.findViewById(R.id.password);
+
 
         signupLink = (TextView) view.findViewById(R.id.signUpLink);
         signupLink.setOnClickListener(new View.OnClickListener() {
@@ -40,11 +46,23 @@ public class LoginFragment extends android.app.Fragment {
                 String username = usernameField.getText().toString();
                 String password = passwordField.getText().toString();
 
-                AuthenticationActivity authenticationActivity = (AuthenticationActivity) getActivity();
-                authenticationActivity.loginUser(username,password);
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    AuthenticationActivity authenticationActivity = (AuthenticationActivity) getActivity();
+                    authenticationActivity.loginUser(username,password);
+                }
+                else {
+                    showErrorMessage("Username and password cannot be empty");
+                }
             }
         });
 
         return view;
+    }
+
+    private void showErrorMessage(String message) {
+        LinearLayout errorMessage = (LinearLayout) view.findViewById(R.id.errorMessage);
+        TextView messageContent = (TextView) view.findViewById(R.id.errorMessageContent);
+        errorMessage.setVisibility(View.VISIBLE);
+        messageContent.setText(message);
     }
 }
