5b1d9bb1dad04430d983b181bd9ea3c5e76fedb6
Michael Swann <n1020368@my.ntu.ac.uk>
2023-12-09 20:07:00 +0000

    WIP nav

--- app/src/main/java/com/example/hypermile/MainActivity.java
+++ app/src/main/java/com/example/hypermile/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.hypermile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int item_id = item.getItemId();
+
+        if (item_id == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, homeFragment).commit();
+        }
+
+        return false;
     }
 }
