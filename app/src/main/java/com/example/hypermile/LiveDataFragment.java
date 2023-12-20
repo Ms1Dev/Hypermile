50ed2b9e62a94c4c49e1bd6703b9d8a248e88ff2
Michael Swann <n1020368@my.ntu.ac.uk>
2023-12-20 19:20:40 +0000

    Created gauge view

--- app/src/main/java/com/example/hypermile/LiveDataFragment.java
+++ app/src/main/java/com/example/hypermile/LiveDataFragment.java
@@ -8,6 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hypermile.visual.GaugeView;
+
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link LiveDataFragment#newInstance} factory method to
@@ -15,33 +19,15 @@ import android.view.ViewGroup;
  */
 public class LiveDataFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    GaugeView gaugeView;
 
     public LiveDataFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LiveDataFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static LiveDataFragment newInstance(String param1, String param2) {
         LiveDataFragment fragment = new LiveDataFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +35,31 @@ public class LiveDataFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_live_data, container, false);
+
+        gaugeView = view.findViewById(R.id.gauge);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        gaugeView.updateValue(random.nextInt(100));
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }).start();
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/hypermile/visual/GaugeView.java
@@ -0,0 +1,145 @@
+package com.example.hypermile.visual;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.hypermile.R;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+public class GaugeView extends RelativeLayout {
+    private final static int DIAL_RANGE = 75;
+    private final static int NORMAL_TEXT_SIZE = 25;
+    private final static int NORMAL_LABEL_SIZE = 11;
+    private final static int NORMAL_DIAL_SIZE = 250;
+    private final static int NORMAL_SIZE = 400;
+    private CircularProgressIndicator dial;
+    private CircularProgressIndicator dialTrack;
+    private TextView label;
+    private TextView unitLabel;
+    int value = 0;
+    int min = 0;
+    int max = 100;
+    View view;
+
+    public GaugeView(Context context) {
+        super(context);
+        initialise(context, null);
+    }
+
+    public GaugeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialise(context, attrs);
+    }
+
+    public GaugeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialise(context, attrs);
+    }
+
+    private void initialise(Context context, AttributeSet attrs) {
+        view = LayoutInflater.from(context).inflate(R.layout.gauge_view, this);
+        dial = view.findViewById(R.id.dial);
+        dialTrack = view.findViewById(R.id.dial_track);
+        label = view.findViewById(R.id.label);
+        unitLabel = view.findViewById(R.id.unit_label);
+
+        if (attrs != null) {
+            TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.GaugeView);
+            int width = attributes.getDimensionPixelSize(R.styleable.GaugeView_width, NORMAL_SIZE);
+            int height = attributes.getDimensionPixelSize(R.styleable.GaugeView_height, NORMAL_SIZE);
+            int scale = Math.min(width,height);
+
+            ConstraintLayout rootLayout = view.findViewById(R.id.gauge_root_layout);
+            LayoutParams rootLayoutParams = (LayoutParams) rootLayout.getLayoutParams();
+            rootLayoutParams.height = height;
+            rootLayoutParams.width = width;
+            rootLayout.setLayoutParams(rootLayoutParams);
+
+            double scaleMultiplier = (scale * 1.0) / NORMAL_SIZE;
+            dial.setIndicatorSize((int) (NORMAL_DIAL_SIZE * scaleMultiplier));
+            dialTrack.setIndicatorSize((int) (NORMAL_DIAL_SIZE * scaleMultiplier));
+            label.setTextSize((float) (NORMAL_TEXT_SIZE * scaleMultiplier));
+            unitLabel.setTextSize((float) (NORMAL_LABEL_SIZE * scaleMultiplier));
+
+            Resources resources = view.getResources();
+
+            int dialColor = attributes.getColor(R.styleable.GaugeView_dialColour, resources.getColor(R.color.primary));
+            int trackColor = attributes.getColor(R.styleable.GaugeView_trackColour, resources.getColor(R.color.secondary_container));
+            int backgroundColor = attributes.getColor(R.styleable.GaugeView_backgroundColour, resources.getColor(R.color.white));
+            int labelColor = attributes.getColor(R.styleable.GaugeView_labelColour, resources.getColor(R.color.black));
+
+            setDialColour(dialColor);
+            setTrackColour(trackColor);
+            setBackgroundColour(backgroundColor);
+            setLabelColour(labelColor);
+
+            String unitLabel = attributes.getString(R.styleable.GaugeView_unitLabel);
+            setUnit(unitLabel);
+
+            if (attributes.getBoolean(R.styleable.GaugeView_hideDial, false)) {
+                hideDial();
+            }
+        }
+    }
+
+    private int convertToDialValue(int value) {
+        int convertedValue = ((value - min) * DIAL_RANGE) / (max - min);
+        if (convertedValue > DIAL_RANGE) convertedValue = DIAL_RANGE;
+        else if (convertedValue < 0) convertedValue = 0;
+        return convertedValue;
+    }
+
+    public void updateValue(int value) {
+        ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                label.setText(String.valueOf(value));
+                dial.setProgress(convertToDialValue(value));
+            }
+        });
+    }
+
+    public void setRange(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setUnit(String unit) {
+        if (unit != null) {
+            unitLabel.setText(unit);
+        }
+    }
+
+    public void setDialColour(int colour) {
+        dial.setIndicatorColor(colour);
+    }
+
+    public void setTrackColour(int colour) {
+        dialTrack.setIndicatorColor(colour);
+    }
+
+    public void setBackgroundColour(int colour) {
+        view.findViewById(R.id.gauge_root_layout).setBackgroundColor(colour);
+    }
+
+    public void setLabelColour(int colour) {
+        label.setTextColor(colour);
+        unitLabel.setTextColor(colour);
+    }
+
+    public void hideDial() {
+        dial.setVisibility(View.GONE);
+        dialTrack.setVisibility(View.GONE);
+    }
+
+}
